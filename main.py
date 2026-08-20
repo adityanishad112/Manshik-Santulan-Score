005d5da3a2be79abdedf91fff7273b11ec04f8f9
aditya <adityanishad112@gmail.com>
2026-08-20 21:57:50 +0530

    updated js file with deployed backend link

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class PredictionResponse(BaseModel):
 
 @app.get('/')
 def greet():
-    return {'Welcome to Aditya: This is a Mental Health Predictor API '}
+    return {'Welcome to Aditya AI: This is a Mental Health Predictor API '}
 
 
 @app.post('/predict', response_model=PredictionResponse) #6.77777
